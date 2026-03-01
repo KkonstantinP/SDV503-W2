525f596ab3a6759bdbf36cdef48044d3d24c6d25
KkonstantinP <Konstantin-pastushko@live.nmit.ac.nz>
2026-03-02 11:24:51 +1300

    changed app js file added home adress

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var name = "Alice";  //Function-scoped, housted - aboid in modern JS
 let age = 25;        // Block-scoped, can be reassigned
 const PI = 3.14159;  //Block-scoped, cannot be reassigned
+
 let score = 0;
 score =10;           // OK- reassignment allowed for let
+
 const Max = 100;
 // MAX = 200;        //type error: assisnment to constant variable
+
+let homeAdress = "Not my home adress" // declared variable called homeAdress and assigned a string value to it of Not my own home adress
